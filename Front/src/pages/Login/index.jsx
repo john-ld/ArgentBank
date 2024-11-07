b5b854f82dbfd831c6f69c72a479fa58359eaa6f
john-ld <153510178+john-ld@users.noreply.github.com>
2024-11-07 17:37:57 +0100

    creation form jsx

--- Front/src/pages/Login/index.jsx
+++ Front/src/pages/Login/index.jsx
@@ -1,4 +1,5 @@
 import DocumentTitle from "../../components/DocumentTitle";
+import { Form } from "../../components/Form/Form";
 
 function Login() {
   const title = "Sign in";
@@ -6,7 +7,11 @@ function Login() {
     <>
       <DocumentTitle title={title} />
       <main className="main bg-dark">
-        <section className="sign-in-content"></section>
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>{title}</h1>
+          <Form />
+        </section>
       </main>
     </>
   );
