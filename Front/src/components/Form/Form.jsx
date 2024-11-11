be3851d59c8f2ad0c80ee5dff68cca000c9f6e58
john-ld <153510178+john-ld@users.noreply.github.com>
2024-11-11 16:21:10 +0100

    creating login page

--- Front/src/components/Form/Form.jsx
+++ Front/src/components/Form/Form.jsx
@@ -1,3 +1,95 @@
- export const Form = () => {
-    return null
-}
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const userLogin = async (payload) => {
+  let response = await fetch("http://localhost:3001/api/v1/user/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: payload,
+  });
+  if (!response.ok) {
+    const error = await response.json();
+    if (response.status === 400) {
+      return { error: "Invalid username or password." };
+    } else {
+      throw new Error(error.message || "Network response was not ok");
+    }
+  }
+  const data = await response.json();
+  return data;
+};
+
+export const Form = () => {
+  const form = useRef();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const loginData = {
+      email,
+      password,
+      rememberMe,
+    };
+    const payload = JSON.stringify(loginData);
+    try {
+      const data = await userLogin(payload);
+      if (data.body.token) {
+        // dispach login //
+        // fetchUser //
+        // dispach setUser//
+        if (loginData.rememberMe) {
+          localStorage.setItem("token", data.body.token);
+          sessionStorage.removeItem("token");
+        } else {
+          sessionStorage.setItem("token", data.body.token);
+          localStorage.removeItem("token");
+        }
+        navigate("/user");
+      } else {
+        console.error("Token not found in response:", data);
+      }
+    } catch (error) {
+      setErrorMessage("Invalid username or password.");
+      console.error("Invalid username or password.", error);
+    }
+  };
+
+  return (
+    <form ref={form} onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Mot de passe:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="rememberMe">Remember me</label>
+        <input
+          type="checkbox"
+          id="rememberMe"
+          checked={rememberMe}
+          onChange={(e) => setRememberMe(e.target.checked)}
+        />
+      </div>
+      <span> {errorMessage}</span>
+      <button type="submit">Se connecter</button>
+    </form>
+  );
+};
